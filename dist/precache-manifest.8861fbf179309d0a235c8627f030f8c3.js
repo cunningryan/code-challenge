f82df93e6295b10855a319bafd9b8f6e5b48cf51
Ryan Cunningham <cham.ryan@gmail.com>
2019-04-01 08:39:29 -0400

    Add dist folder.

---
+++ dist/precache-manifest.8861fbf179309d0a235c8627f030f8c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2e7e7892ba9fa219ee52",
+    "url": "/static/js/main.32b10970.chunk.js"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "2e7e7892ba9fa219ee52",
+    "url": "/static/css/main.134335b2.chunk.css"
+  },
+  {
+    "revision": "8c4e3d903dd1fa37c5c548e1dac9fc0e",
+    "url": "/index.html"
+  }
+];
