0e77b75bc3b69747b0b33cbcfc48f4ed4e261b8f
Ryan Cunningham <cham.ryan@gmail.com>
2019-04-01 08:38:24 -0400

    And close the nav on click for good measure.

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -24,7 +24,7 @@ const Header = () => {
           <span className="navbar-toggler-icon" />
         </button>
         <div className={`collapse navbar-collapse ${showNav ? "show" : ""}`}>
-          <ul className="navbar-nav mr-auto">
+          <ul className="navbar-nav mr-auto" onClick={() => setShowNav(false)}>
             {navLinks.map(linkObj => (
               <NavItem
                 title={linkObj.title}
